84c237a1f041797f8fc38f60e48913e97810be84
Nikola <nikola.tutic@usherbrooke.ca>
2022-08-24 14:48:35 -0400

    Added conditional downloading if file not found

--- golang/fetchHsTp.go
+++ golang/fetchHsTp.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
 )
 
 func FetchHsTp(dataPath, resolution, year, month string) error {
 	yearMonth := year + month
 	stats := [2]string{"hs", "tp"}
 	var err error
+	var matches []string
 
 	for _, stat := range stats {
 		file := "multi_reanal.glo_" + resolution + "_ext." + stat + "." + yearMonth + ".grb2"
-		fileUrl := "https://polar.ncep.noaa.gov/waves/hindcasts/nopp-phase2/" + yearMonth + "/gribs/" + file
-		err = Download(dataPath+file, fileUrl)
-		if err != nil {
-			panic(err)
+		filePath := filepath.Join(dataPath, file)
+
+		matches, err = filepath.Glob(filePath)
+
+		if matches == nil {
+			fileUrl := "https://polar.ncep.noaa.gov/waves/hindcasts/nopp-phase2/" + yearMonth + "/gribs/" + file
+			err = Download(filePath, fileUrl)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Downloaded: " + file)
+		} else {
+			fmt.Println("Found: " + matches[0])
 		}
-		fmt.Println("Downloaded: " + file)
+
 	}
 
 	return err
